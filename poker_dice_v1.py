9845b4fd42d0bdd75e20eebcaa9bded26b70952c
Arnaldo Sandoval <tinaco110@hotmail.com>
2020-09-03 11:47:31 +1000

    Update poker_dice_v1.py
    = as coded in the Python Book Magazine

--- poker_dice_v1.py
+++ poker_dice_v1.py
@@ -1,24 +1,13 @@
 # Linux: poker_dice game version 1
 
+# Version 1 features:
+# ~~~~~~~~~~~~~~~~~~
+# + Single user version.
+# + It does not implement the computer user
+#
+
 import random
-import time
 from itertools import groupby
-
-
-class BgColors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    Reset = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    Green = '\u001b[38;5;28m'
-    Yellow = '\u001b[38;5;3m'
-    Blue = '\u001b[38;5;4m'
-    DarkBlue = '\u001b[38;5;21m'
-
 
 # initialize rules
 nine = 1
@@ -32,9 +21,6 @@ names = {nine: "9", ten: "10", jack: "J", queen: "Q", king: "K", ace: "A"}
 
 player_score = 0
 computer_score = 0
-draws = 0
-
-verbose = False
 
 # who are you?
 user_name = input("What is your name? ")
@@ -42,113 +28,70 @@ computer_hand = 0
 
 
 def start():
-    print(f"{BgColors.Blue}Welcome {user_name}, let's play a game of Linux's Poker Dice{BgColors.Reset}")
+    print("Let's play a game of Linux's Poker Dice")
     while game():
         pass
     scores()
 
 
 def game():
-    global player_score, computer_score, draws
-    print(f"{user_name}, the computer will help you throw the 5 dices")
-    result = throw(False)
-    user_hand = result[1]
-
-    print(f"{BgColors.Blue}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{BgColors.Reset}")
-    print(f"{BgColors.Blue}The Computer is playing now!{BgColors.Reset}")
-    time.sleep(2)
-
-    result = throw(True)
-    if user_hand > result[1]:
-        print(f"You won with a hand of {user_hand}")
-        player_score += 1
-    elif user_hand < result[1]:
-        print(f"You lost, your hand is {user_hand}, computer hand {result[1]}")
-        computer_score += 1
-    else:
-        print(f"It is a draw, a hand with a value of {user_hand}")
-        draws += 1
+    print("The computer will help you throw the 5 dices")
+    result = throw()
     return play_again()
 
 
-def throw(is_computer):
-    user = user_name
+def throw():
     dice_to_roll_idx = 0
-    if is_computer:
-        user = f"{BgColors.Blue}Computer{BgColors.Reset}"
 
-    roll_times = 5
-    dice = roll(roll_times)
+    roll_number = 5
+    dice = roll(roll_number)
+    dice.sort()
+    for i in range(len(dice)):
+        print("Dice ", i + 1, ":", names[dice[i]])
+    result = hand(dice)
+    print("You currently have ", result)
 
-    result = show_hand(dice)
-    print(f"{user}, you currently have '{result[0]}' with a value {result[1]}")
+    while True:
+        rerolls = input("How many dice do you want to throw again? ")
+        try:
+            if rerolls in (1,2,3,4,5):
+                break
+        except ValueError:
+            print("Oops! I didn't understand that. Please enter: 1, 2, 3, 4 or 5")
 
-    if is_computer:
-        print(f"{user} {BgColors.Yellow} is rolling {result[2]} dices {BgColors.Reset}")
-        roll_times = result[2]
+    if rerolls == 0:
+        print("You finish with ", result)
     else:
-        roll_times = get_dices(user)
-    if roll_times == 0:
-        print(f"{user} You finish with {result[0]}")
-    else:
-        #    roll_times = dice_quantity
-        dices_new_hand = roll(roll_times)
-        dice_changes = list(range(roll_times))
-        if verbose:
-            print(f"Rolled {roll_times} dices")
-            print(f"New dices {dice_changes}")
-            print(f"Dices new {dices_new_hand}")
-
-        print("Enter the number of a dice to re_roll? ")
+        roll_number = rerolls
+        dice_rerolls = roll(roll_number)
+        dice_changes = range(rerolls)
+        print("Enter the number of a dice to reroll: ")
         iterations = 0
-        while iterations < roll_times:
-            iterations += 1
+        while iterations < rerolls:
+            iterations = iterations + 1
             while True:
-                if is_computer:
-                    while True:
-                        if dice.count(dice[dice_to_roll_idx]) == 1:
-                            dice_to_roll_idx += 1
-                            selection = dice_to_roll_idx
-                            break
-                        else:
-                            dice_to_roll_idx += 1
-                    print(f"Next dice? {BgColors.Green}{selection}{BgColors.Reset}")
-                else:
-                    selection = int(input("Next dice? "))
-                try:
-                    if selection in (1, 2, 3, 4, 5):
+                selection = input("? ")
+                try
+                    if selection in (1,2,3,4,5):
                         break
                 except ValueError:
                     pass
                 print("Oops! I didn't understand that. Please enter: 1, 2, 3, 4 or 5")
             dice_changes[iterations - 1] = selection - 1
-            print(f"You have changed dice {selection} with {dice_changes[iterations - 1]}")
+            print("You have changed dice ", selection)
 
-        print(f"After rolling {dice_changes}")
         iterations = 0
-        while iterations < roll_times:
-            iterations += 1
-            replacement = dices_new_hand[iterations - 1]
+        while iterations < rerolls:
+            iterations = iterations + 1
+            replacement = dice_rerolls[iterations - 1]
             dice[dice_changes[iterations - 1]] = replacement
 
-        result = show_hand(dice)
-        print(f"{user}, You currently have '{result[0]}' with a value {result[1]}")
-    return result
+        dice.sort()
+        for i in range(len(dice)):
+            print("Dice ", i + 1, ":", names[dice[i]])
 
-
-def get_dices(user):
-    while True:
-        try:
-            dices = int(input(f"{user}, How many dices do you want to throw again? "))
-            if dices in (0, 1, 2, 3, 4, 5):
-                print(f"Dices {dices}")
-                return dices
-            else:
-                print("Please enter: 0, 1, 2, 3, 4 or 5")
-
-        except ValueError:
-            print("Oops! I don't understand that. Please enter 0, 1, 2, 3, 4 or 5")
-            pass
+        result = hand(dice)
+        print("You finish with: ", result)
 
 
 def roll(roll_number):
@@ -156,70 +99,35 @@ def roll(roll_number):
     dice = list(range(roll_number))
     iterations = 0
     while iterations < roll_number:
-        iterations += 1
+        iterations = iterations + 1
         dice[iterations - 1] = random.choice(numbers)
     return dice
 
 
-def show_hand(dice) -> object:
-    hand_result = []
-
-    # showing hand
-    dice.sort()
-    for d in range(len(dice)):
-        print(f"Dice {d + 1} : {names[dice[d]]}")
-
-    # hand result
+def hand(dice)
     dice_hand = [len(list(group)) for key, group in groupby(dice)]
-    dice_hand.sort()
+    dice_hand.sort(reverse=True)
     straight1 = [1, 2, 3, 4, 5]
     straight2 = [2, 3, 4, 5, 6]
 
-    print(f"Dice Hand {dice_hand}")
-
     if dice == straight1 or dice == straight2:
-        hand_result.append("a straight!")
-        hand_result.append(5)  # hand value
-        hand_result.append(0)  # new cards
-        # return hand_result
+        return "a straight!"
     elif dice_hand[0] == 5:
-        hand_result.append("five of a kind!")
-        hand_result.append(8)  # hand value
-        hand_result.append(0)  # new cards
-        # return hand_result
+        return "five of a kind!"
     elif dice_hand[0] == 4:
-        hand_result.append("four of a kind!")
-        hand_result.append(7)
-        hand_result.append(1)
-        # return hand_result
-    elif (dice_hand[0] == 3 and dice_hand[1] == 2) or (dice_hand[0] == 2 and dice_hand[1] == 3):
-        hand_result.append("a full house!")
-        hand_result.append(6)
-        hand_result.append(0)
-        # return hand_result
-    elif (dice_hand[0] == 3 and dice_hand[1] == 1) or (dice_hand[0] == 1 and dice_hand[1] == 3) or (
-            dice_hand[0] == 1 and dice_hand[1] == 1 and dice_hand[2] == 3):
-        hand_result.append("three of a kind!")
-        hand_result.append(4)
-        hand_result.append(2)
-        # return hand_result
-    elif (dice_hand[0] == 2 and dice_hand[1] == 2) or (dice_hand[0] == 1 and dice_hand[1] == 2):
-        hand_result.append("two pairs!")
-        hand_result.append(3)
-        hand_result.append(1)
-        # return hand_result
-    elif (dice_hand[0] == 2 and dice_hand[1] == 1 and dice_hand[2] == 1) or (
-            dice_hand[0] == 1 and dice_hand[1] == 2 and dice_hand[2] == 1) or (
-            dice_hand[0] == 1 and dice_hand[1] == 1 and dice_hand[2] == 1 and dice_hand[3] == 2):
-        hand_result.append("one pair!")
-        hand_result.append(2)
-        hand_result.append(3)
-        # return hand_result
+        return "four of a kind!"
+    elif dice_hand[0] == 3:
+        if dice_hand[1] == 2:
+            return "a full house!"
+        else:
+            return "three of a kind!"
+    elif dice_hand[0] == 2:
+        if dice_hand[1] == 2:
+            return "two pairs!"
+        else:
+            return "one pair!"
     else:
-        hand_result.append("a high card")
-        hand_result.append(1)
-        hand_result.append(4)
-    return hand_result
+        return "a high card"
 
 
 def play_again():
@@ -231,11 +139,10 @@ def play_again():
 
 
 def scores():
-    global player_score, computer_score, draws
+    global player_score, computer_score
     print("HIGH SCORES")
-    print(f"Player  : {player_score}")
-    print(f"Computer: {computer_score}")
-    print(f"Draws   : {draws}")
+    print("Player  : ", player_score)
+    print("Computer: ", computer_score)
 
 
 # Press the green button in the gutter to run the script.
